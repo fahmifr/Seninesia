a183c386f31b975bbab98a76edddc9e5b0961560
fahmifr <fahmi.fadhurrozi98@gmail.com>
2019-10-29 20:42:17 +0700

    ubah database migration

--- database/migrations/2019_10_23_005136_create_seni_table.php
+++ database/migrations/2019_10_23_005136_create_seni_table.php
@@ -13,7 +13,7 @@ class CreateSeniTable extends Migration
      */
     public function up()
     {
-        Schema::create('Seni', function (Blueprint $table) {
+        Schema::create('seni', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
             $table->string('description');
